8de981b14191e32ee8742fae5640cf2d0541fbc8
Andrew Wilson <drbobchoco@gmail.com>
2024-11-13 20:11:05 +0000

    Home-row mods for DrBobChoco Lily58 keymap

--- keyboards/lily58/keymaps/DrBobChoco/keymap.c
+++ keyboards/lily58/keymaps/DrBobChoco/keymap.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  =   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |LShift|   A  | Ct/S | GU/D | Al/F | Sh/G |-------.    ,-------| Sh/H | Al/J | GU/K | Ct/L |   ;  |  '   |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |LCTRL |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  \   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -26,8 +26,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT( \
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_EQL, \
-  KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, \
-  KC_LCTRL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_BSLS, \
+  KC_LSFT, KC_A, LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), LSFT_T(KC_G),  RSFT_T(KC_H), RGUI_T(KC_J), RALT_T(KC_K), RCTL_T(KC_L), KC_SCLN, KC_QUOT, \
+  KC_LCTL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_BSLS, \
                         KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC, KC_ENT, MO(_RAISE), KC_INS, KC_BSPC \
 ),
 
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT( \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
   _______, XXXXXXX, KC_BTN3, KC_BTN2, KC_BTN1, KC_WH_U,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, KC_F12,\
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_D,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TILD, KC_PGUP, \
+  _______, XXXXXXX, KC_LCTL, KC_LALT, KC_LGUI, LSFT_T(KC_WH_D),  RSFT_T(KC_LEFT), RGUI_T(KC_DOWN), RALT_T(KC_UP), RCTL_T(KC_RGHT), KC_TILD, KC_PGUP, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGDN, \
                              _______, _______, _______, _______, _______,  _______, _______, KC_DEL\
 ),
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT( \
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_U,                   XXXXXXX, KC_BTN1, KC_BTN2, KC_BTN3, XXXXXXX, KC_F12,\
-  _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_D,                   KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT, KC_GRV,  KC_HOME, \
+  _______, XXXXXXX, KC_LCTL, KC_LALT, KC_LGUI, LSFT_T(KC_WH_D),  RSFT_T(KC_MPLY), RGUI_T(KC_VOLD), RALT_T(KC_VOLU), RCTL_T(KC_MNXT), KC_GRV, KC_HOME, \
   _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_END, \
                              _______, _______, _______,  _______, _______,  _______, _______, _______ \
 ),
